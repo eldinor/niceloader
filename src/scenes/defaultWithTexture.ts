ac629eccdf1d276800259b58fc451eb260222390
eldinor <eldinor@gmail.com>
2023-02-05 19:05:55 +0300

    Io added

--- src/scenes/defaultWithTexture.ts
+++ src/scenes/defaultWithTexture.ts
@@ -6,8 +6,17 @@ import { CreateSphere } from "@babylonjs/core/Meshes/Builders/sphereBuilder";
 import { CreateGround } from "@babylonjs/core/Meshes/Builders/groundBuilder";
 import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
 import { CubeTexture } from "@babylonjs/core/Materials";
+import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+import "@babylonjs/loaders/glTF";
 
 import { CreateSceneClass } from "../createScene";
+
+import * as GT from "@gltf-transform/core";
+import { dedup, draco, prune, quantize, weld } from "@gltf-transform/functions";
+import { KHRONOS_EXTENSIONS } from "@gltf-transform/extensions";
+
+import { MeshoptEncoder } from "meshoptimizer";
+import { reorder } from "@gltf-transform/functions";
 
 // If you don't need the standard material you will still need to import it since the scene requires it.
 // import "@babylonjs/core/Materials/standardMaterial";
@@ -59,7 +68,7 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
       0,
       Math.PI / 3,
       10,
-      new Vector3(0, 0, 0),
+      new Vector3(0, 1, 0),
       scene
     );
 
@@ -116,7 +125,61 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
         shadowGenerator.getShadowMap()!.renderList!.push(sphere);
 
 */
+    const importResult = await SceneLoader.ImportMeshAsync(
+      "",
+      "/",
+      "walls.glb",
+      scene
+    );
+    importResult.meshes[0].scaling.scaleInPlace(0.05);
+    importResult.meshes.forEach((m) => {
+      m.checkCollisions = true;
+    });
+    //
+    console.log(importResult);
 
+    async function ggg() {
+      const io = new GT.WebIO().registerExtensions(KHRONOS_EXTENSIONS);
+      console.log(io);
+
+      const doc = await io.read("walls.glb");
+      console.log(doc);
+
+      const firstuint = await io.writeBinary(doc);
+      console.log(firstuint);
+
+      await MeshoptEncoder.ready;
+
+      const tt = await doc.transform(
+        prune(),
+        weld(),
+        quantize(),
+        dedup(),
+        reorder({ encoder: MeshoptEncoder })
+      );
+
+      console.log(reorder({ encoder: MeshoptEncoder, target: "size" }));
+      console.log(tt);
+
+      let uint = await io.writeBinary(tt);
+      console.log(uint);
+
+      console.log(uint.buffer);
+
+      const assetBlob = new Blob([uint.buffer]);
+      const assetUrl = URL.createObjectURL(assetBlob);
+
+      await SceneLoader.AppendAsync(
+        assetUrl,
+        undefined,
+        scene,
+        undefined,
+        ".glb"
+      );
+    }
+
+    await ggg();
+    //
     new NiceLoader(scene, modelsArray);
 
     return scene;
