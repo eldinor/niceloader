36b8bcb07ff431d0e218a3632db277b16d47c2ce
eldinor <eldinor@gmail.com>
2023-02-08 16:28:35 +0300

    marble tower

--- src/scenes/cc2.ts
+++ src/scenes/cc2.ts
@@ -11,10 +11,15 @@ import { Tools } from "@babylonjs/core/Misc/tools";
 import { AnimationGroup } from "@babylonjs/core/Animations/animationGroup";
 import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
 import {
+  HingeJoint,
   Mesh,
   PBRMaterial,
   RayHelper,
   ReflectionProbe,
+  TransformNode,
+  Animation,
+  ActionManager,
+  ExecuteCodeAction,
 } from "@babylonjs/core/";
 import { mainPipeline } from "../externals/Pipeline";
 
@@ -97,31 +102,31 @@ class PhysicsSceneWithAmmo implements CreateSceneClass {
 
     const sphere = MeshBuilder.CreateSphere(
       "sphere",
-      { diameter: 2, segments: 32 },
+      { diameter: 1, segments: 32 },
       scene
     );
     sphere.material = new PBRMaterial("spMat", scene);
     (sphere.material as PBRMaterial).roughness = 0.2;
 
     sphere.checkCollisions = true;
-    /*
+
     for (let i = 0; i < 5; i++) {
       const spClone = sphere.clone("spClone");
 
       spClone.physicsImpostor = new PhysicsImpostor(
         spClone,
         PhysicsImpostor.SphereImpostor,
-        { mass: 0.1, restitution: 0.2 },
+        { mass: 0.1, restitution: 0.6 },
         scene
       );
 
       // Move the sphere upward 1/2 its height
-      spClone.position.y = 9;
-      spClone.position.x = -5;
+      spClone.position.y = 19;
+      spClone.position.z = -5;
 
       spClone.checkCollisions = true;
     }
-*/
+
     for (let i = 0; i < 20; i++) {
       const box = MeshBuilder.CreateBox("box", { size: 1 }, scene);
       box.material = sphere.material;
@@ -194,9 +199,13 @@ class PhysicsSceneWithAmmo implements CreateSceneClass {
       "walls.glb",
       scene
     );
+
+    importResult.meshes[0].position.y = -40;
+    //
     importResult.meshes[0].scaling.scaleInPlace(0.4);
     importResult.meshes.forEach((m) => {
       m.checkCollisions = true;
+      m.isPickable = false;
     });
 
     console.log(importResult);
@@ -207,7 +216,8 @@ class PhysicsSceneWithAmmo implements CreateSceneClass {
       { width: 60, height: 60 },
       scene
     );
-    ground.position.y = 0.1;
+    ground.position.y = -40.1;
+    // ground.position.y = 0.1;
 
     ground.physicsImpostor = new PhysicsImpostor(
       ground,
@@ -511,6 +521,265 @@ class PhysicsSceneWithAmmo implements CreateSceneClass {
     //
     // mainPipeline(scene, camera);
     //
+    //create an array of different starting positions for the marbles
+    var marbleStartPosArray = [
+      new Vector3(0.2, 3.5, 0),
+      new Vector3(0, 3.5, 0.2),
+      new Vector3(-0.2, 3.5, 0),
+      new Vector3(0, 3.5, -0.2),
+    ];
+
+    //create a box used to trigger the destrucion of marbles
+    var killBox = MeshBuilder.CreateBox(
+      "killBox",
+      { width: 100, depth: 100, height: 0.5 },
+      scene
+    );
+    killBox.position = new Vector3(0, -50, 0);
+    killBox.visibility = 0;
+
+    var marbleMaterialArray = [];
+    //
+    Promise.all([
+      SceneLoader.AppendAsync(
+        "https://models.babylonjs.com/Marble/marble/marble.gltf"
+      ),
+      SceneLoader.AppendAsync(
+        "https://models.babylonjs.com/Marble/marbleTower/marbleTower.gltf"
+      ),
+    ]).then(function () {
+      var marble = scene.getMeshByName("marble");
+      marble.setParent(null);
+      marble.visibility = 0;
+
+      marbleMaterialArray.push(
+        scene.getMaterialByName("blueMat"),
+        scene.getMaterialByName("greenMat"),
+        scene.getMaterialByName("redMat"),
+        scene.getMaterialByName("purpleMat"),
+        scene.getMaterialByName("yellowMat")
+      );
+
+      //get each mesh that's been loaded
+      var tower = scene.getMeshByName("tower");
+      var rockerBottom = scene.getMeshByName("rockerBottom");
+      var rockerTop = scene.getMeshByName("rockerTop");
+      var spinner = scene.getMeshByName("spinner");
+      var supports = scene.getMeshByName("supports");
+      var track = scene.getMeshByName("track");
+      var wheel = scene.getMeshByName("wheel");
+
+      //set the parents of each mesh to null
+      tower.setParent(null);
+      rockerBottom.setParent(null);
+      rockerTop.setParent(null);
+      spinner.setParent(null);
+      supports.setParent(null);
+      track.setParent(null);
+      wheel.setParent(null);
+
+      //add physics imposters to anything marbles will collide with
+      tower.physicsImpostor = new PhysicsImpostor(
+        tower,
+        PhysicsImpostor.MeshImpostor,
+        { mass: 0, friction: 1 },
+        scene
+      );
+      supports.physicsImpostor = new PhysicsImpostor(
+        supports,
+        PhysicsImpostor.MeshImpostor,
+        { mass: 0, friction: 1 },
+        scene
+      );
+      track.physicsImpostor = new PhysicsImpostor(
+        track,
+        PhysicsImpostor.MeshImpostor,
+        { mass: 0, friction: 1 },
+        scene
+      );
+      wheel.physicsImpostor = new PhysicsImpostor(
+        wheel,
+        PhysicsImpostor.MeshImpostor,
+        { mass: 0, friction: 1 },
+        scene
+      );
+
+      //setup the rocker
+
+      // Create rocker pin as the phsyics root and parent loaded assets to it
+      var rockerRoot = new Mesh("rockerRoot", scene);
+      rockerBottom.setParent(rockerRoot);
+      rockerTop.setParent(rockerRoot);
+      rockerRoot.position = new Vector3(4.1, -6.4, 0);
+      rockerRoot.rotation.x -= Tools.ToRadians(25);
+
+      rockerTop.physicsImpostor = new PhysicsImpostor(
+        rockerTop,
+        PhysicsImpostor.ConvexHullImpostor,
+        { mass: 0 },
+        scene
+      );
+      rockerBottom.physicsImpostor = new PhysicsImpostor(
+        rockerBottom,
+        PhysicsImpostor.ConvexHullImpostor,
+        { mass: 0 },
+        scene
+      );
+      rockerRoot.physicsImpostor = new PhysicsImpostor(
+        rockerRoot,
+        PhysicsImpostor.NoImpostor,
+        { mass: 2 },
+        scene
+      );
+
+      var rockerPin = MeshBuilder.CreateCylinder(
+        "Rocker",
+        { diameter: 0.1, height: 1 },
+        scene
+      );
+      rockerPin.rotation.z += Tools.ToRadians(90);
+      rockerPin.position = new Vector3(4.1, -6.4, 0);
+      rockerPin.physicsImpostor = new PhysicsImpostor(
+        rockerPin,
+        PhysicsImpostor.MeshImpostor,
+        { mass: 0 },
+        scene
+      );
+      rockerPin.visibility = 0;
+
+      var joint1 = new HingeJoint({
+        mainPivot: new Vector3(0, 0, 0),
+        connectedPivot: new Vector3(0, 0, 0),
+        mainAxis: new Vector3(-1, 0, 0),
+        connectedAxis: new Vector3(0, 1, 0),
+        nativeParams: {},
+      });
+      rockerRoot.physicsImpostor.addJoint(rockerPin.physicsImpostor, joint1);
+
+      //handle logic for the brass wind-up spinner
+      var currentWindUpAngle;
+      var marbleSpawnRate = 8;
+      var nextMarbleSpawnAngle = 360 / marbleSpawnRate;
+      var spinnerRotateSpeed = 120;
+      var marblePosition = 0;
+
+      var spinnerPivotParent = new TransformNode("spinnerPivotParent");
+      spinner.setParent(spinnerPivotParent);
+
+      Animation.CreateAndStartAnimation(
+        "spinnerRotation",
+        spinnerPivotParent,
+        "rotation.y",
+        30,
+        spinnerRotateSpeed,
+        Tools.ToRadians(0),
+        Tools.ToRadians(360),
+        1
+      );
+
+      //handle logic for the large wheel
+      var wheelPivotParent = new TransformNode("wheelPivotParent");
+      wheelPivotParent.position.y -= 28.8;
+      wheel.setParent(wheelPivotParent);
+      Animation.CreateAndStartAnimation(
+        "marbleTowerWheelRot",
+        wheelPivotParent,
+        "rotation.x",
+        30,
+        600,
+        Tools.ToRadians(0),
+        Tools.ToRadians(-360),
+        1
+      );
+
+      //logic to change the starting marble position based on the rotation of the brass wind-up spinner
+      scene.actionManager = new ActionManager(scene);
+      scene.actionManager.registerAction(
+        new ExecuteCodeAction(
+          {
+            trigger: ActionManager.OnEveryFrameTrigger,
+          },
+          function () {
+            currentWindUpAngle = Tools.ToDegrees(spinnerPivotParent.rotation.y);
+            if (
+              nextMarbleSpawnAngle == 360 &&
+              currentWindUpAngle < 360 / marbleSpawnRate
+            ) {
+              nextMarbleSpawnAngle = 360 / marbleSpawnRate;
+            } else if (currentWindUpAngle >= nextMarbleSpawnAngle) {
+              nextMarbleSpawnAngle += 360 / marbleSpawnRate;
+              createMarble(marblePosition);
+              marblePosition += 1;
+              if (marblePosition == 4) {
+                marblePosition = 0;
+              }
+            }
+          }
+        )
+      );
+
+      //   engine.hideLoadingUI();
+    });
+    ///
+
+    function createMarble(spawnAngle) {
+      //create a marble (sphere) using meshbuilder
+      var marble = scene.getMeshByName("marble").clone("marbleClone", null);
+      marble.visibility = 1;
+      marble.material = marbleMaterialArray[Math.floor(Math.random() * 5)];
+
+      //position the marble based on the incoming angle of the windup part of the marbleTower
+      marble.position = marbleStartPosArray[spawnAngle];
+
+      //add physics to the marble
+      marble.physicsImpostor = new PhysicsImpostor(
+        marble,
+        PhysicsImpostor.SphereImpostor,
+        { mass: 2, friction: 0.5, restitution: 0 },
+        scene
+      );
+
+      //add an actionManager to the marble
+      marble.actionManager = new ActionManager(scene);
+
+      //register a new action with the marble's actionManager..this will execute code whenever the marble intersects the "killBox"
+      marble.actionManager.registerAction(
+        new ExecuteCodeAction(
+          {
+            trigger: ActionManager.OnIntersectionEnterTrigger,
+            parameter: killBox,
+          },
+          function () {
+            fadeAndDestroyMarble(marble);
+          }
+        )
+      );
+    }
+
+    function fadeAndDestroyMarble(marble) {
+      var forceDirection = new Vector3(0, 1, 0);
+      var forceMagnitude = 25;
+      var contactLocalRefPoint = Vector3.Zero();
+
+      //the one line of code version
+      Animation.CreateAndStartAnimation(
+        "marbleVisAnim",
+        marble,
+        "visibility",
+        30,
+        30,
+        1,
+        0,
+        0,
+        null,
+        () => {
+          marble.dispose();
+        }
+      );
+    }
+    //
+    //
+    //
     return scene;
   };
 }
@@ -524,7 +793,7 @@ function checkRayCast(scene: Scene) {
 
   // const rayHelper = new RayHelper(ray);
   // rayHelper.show(scene);
-  // console.log(rayHelper);
+  // console.log(ray);
 
   const pickingInfo = scene.pickWithRay(ray, (m) => {
     return m.isVisible && m.isPickable && m.visibility > 0;
@@ -541,8 +810,8 @@ function checkRayCast(scene: Scene) {
       offsetDistance = distance - (camera as ArcRotateCamera).radius;
     }
     if (pickedMesh) {
-      if (pickedMesh.name!.includes("ox")) {
-        console.log("BOX", ray);
+      if (pickedMesh.name!.includes("ox") || pickedMesh.name!.includes("one")) {
+        //   console.log("BOX", ray);
         pickedMesh.applyImpulse(ray.direction, Vector3.Zero());
         // (pickedMesh.material as PBRMaterial).albedoColor = Color3.Green()
       }
